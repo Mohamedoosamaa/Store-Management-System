58b1259cac1401322e22850bde596fbd14a9a643
Hesham Shahat <hishamshahat06@gmail.com>
2026-05-26 02:54:04 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace dashboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
